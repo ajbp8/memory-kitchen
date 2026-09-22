e4a04d7fcda3fa077a03a2f515c36c66deb9edd9
ajbp8 <adrienpierson8@gmail.com>
2026-09-22 11:18:45 +0800

    Update SplashScreen.tsx

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -15,8 +15,8 @@ export default function SplashScreen() {
     }
 
     setVisible(true);
-    const fadeTimer  = setTimeout(() => setFading(true),  5000);
-    const hideTimer  = setTimeout(() => setVisible(false), 5600);
+    const fadeTimer  = setTimeout(() => setFading(true),  1500);
+    const hideTimer  = setTimeout(() => setVisible(false), 8000);
     return () => { clearTimeout(fadeTimer); clearTimeout(hideTimer); };
   }, []);
 
